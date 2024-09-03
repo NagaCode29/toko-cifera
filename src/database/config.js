86e9f3210a3e18f9e92d4a30ac6a2a6e340b496a
Diory Pribadi Sinaga <dioripribadisinaga@gmail.com>
2024-09-04 02:59:23 +0700

    change port database

--- src/database/config.js
+++ src/database/config.js
@@ -3,4 +3,5 @@ export default {
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.NODE_ENV !== 'test' ? process.env.DATABASE : process.env.DATABASE_TEST,
+    port: process.env.PORT_DATABASE,
 }
